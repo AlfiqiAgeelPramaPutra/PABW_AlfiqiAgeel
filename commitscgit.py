df0d4bca471270287016dc1b04776db916977481
Alfiqi Ageel <alfiqiageelpp1@gmail.com>
2023-03-09 08:58:57 +0700

    perubahan 2

---
+++ commitscgit.py
@@ -0,0 +1,13 @@
+print('Angka 1 to 100:')
+n = 1
+while n <= 100:
+    print(n, end=' ')
+    n = n+1
+    
+    if n % 2 == 0:
+    print(" %d Merupakan Bilangan Genap" % bil)
+    
+    else:
+    print("%d Merupakan Bilangan Ganjil" % bil)
+
+
